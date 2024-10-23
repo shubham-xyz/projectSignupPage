d02564c3f79c4d31aa9bc1732b94edb04a8bd372
shubham-xyz <xyz.allforone@gmail.com>
2024-10-24 04:32:59 +0530

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('infoForm').addEventListener('submit', function(event) {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        event.preventDefault(); // Prevent form submission
+    }
+});
